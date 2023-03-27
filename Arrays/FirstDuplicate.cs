5ee3b38ecff7ac223849b5ba794991122fb29048
Dmitriy <dmitriy.tereshchenko1989@gmail.com>
2023-03-27 19:44:46 +0300

    Добавьте файлы проекта.

---
+++ Arrays/FirstDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodedSignal_Interview_Practice.Arrays
+{
+    internal class FirstDuplicate
+    {
+        public int DuplicateFirst(int[] a)
+        {
+            for (int i = 0; i < a.Length; ++i)
+            {
+                int index = Math.Abs(a[i]) - 1;
+                if (a[index] < 0)
+                {
+                    return Math.Abs(a[i]);
+                }
+                a[index] *= -1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using CodedSignal_Interview_Practice.Arrays;
+
+namespace CodedSignal_Interview_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstDuplicate firstDuplicate = new();
+            Console.WriteLine(firstDuplicate.DuplicateFirst(new int[] { 2, 1, 3, 5, 3, 2 }));
+        }
+    }
+}
